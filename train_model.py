4e53674b18e353f89d23356f2f7638bb5c875746
nathaliejanssen <63008491+nathaliejanssen@users.noreply.github.com>
2021-04-21 16:08:07 +0300

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -18,32 +18,42 @@ def create_image_path_df(path_to_imgfolder):
 
     return df_imgs
 
-def read_imgs(nucleus, brightfield, plane, number_of_slices):
-    start_plane = plane - (number_of_slices // 2)
-    imgs = tf.TensorArray(tf.float32, size = number_of_slices)
+def read_imgs(nucleus, brightfield, plane, number_of_slices, plane_string):
+    # Define plane first image in the stack
+    start = plane - int(number_of_slices/2)
+    
+    if start < 10:
+        start_string = tf.strings.format('p0{}', start)
+    else:
+        start_string = tf.strings.format('p{}', start)
 
+    # Prepare stack of bf images
+    imgs = tf.TensorArray(tf.float32, size=number_of_slices)
+    
     for i in range(number_of_slices):
-        if i == 0:
-            if start_plane < 10:
-                fn = tf.strings.regex_replace(brightfield, tf.strings.format('p0{}', plane) if plane < 10 else tf.strings.format('p{}', plane), tf.strings.format('p0{}', start_plane))
-            else:
-                fn = tf.strings.regex_replace(brightfield, tf.strings.format('p0{}', plane) if plane < 10 else tf.strings.format('p{}', plane), tf.strings.format('p{}', start_plane))
-        else:
-            if start_plane < 10:
-                fn = tf.strings.regex_replace(brightfield, tf.strings.format('p0{}', plane) if plane < 10 else tf.strings.format('p{}', plane), tf.strings.format('p0{}', start_plane+i))
-            else:
-                fn = tf.strings.regex_replace(brightfield, tf.strings.format('p0{}', plane) if plane < 10 else tf.strings.format('p{}', plane), tf.strings.format('p{}', start_plane+i))
+        fn = tf.strings.regex_replace(brightfield, plane_string, start_string)
 
-        img = tf.io.decode_png(tf.io.read_file(fn), channels = 1, dtype = tf.uint16) 
+        if i > 0:
+            next_plane = start+i
+            if next_plane < 10:
+                next_plane = tf.strings.format('p0{}', next_plane)
+            else:
+                next_plane = tf.strings.format('p{}', next_plane)
+            
+            fn = tf.strings.regex_replace(fn, start_string, next_plane)
+
+        img = tf.io.decode_png(tf.io.read_file(fn), channels=1, dtype=tf.uint16) 
         img = tf.image.convert_image_dtype(img, tf.float32)
-        img = tf.image.resize_with_crop_or_pad(img, 848, 848)   # otherwise it won't work in the current unet architecture
+        img = tf.image.resize_with_crop_or_pad(img, 848, 848) 
         imgs = imgs.write(i, tf.squeeze(img))
-    bfimg = imgs.stack()    
-    bfimg = tf.transpose(bfimg, perm = [1,2,0])
 
-    fluo_target = tf.io.decode_png(tf.io.read_file(nucleus), channels = 1,dtype = tf.uint16)
+    bfimg = imgs.stack()    
+    bfimg = tf.transpose(bfimg, perm=[1,2,0])
+
+    # Prepare fluorescent target
+    fluo_target = tf.io.decode_png(tf.io.read_file(nucleus), channels=1,dtype=tf.uint16)
     fluo_target = tf.image.convert_image_dtype(fluo_target, tf.float32)
-    fluo_target = tf.image.resize_with_crop_or_pad(fluo_target, 848, 848)   # otherwise it won't work in the current unet architecture
+    fluo_target = tf.image.resize_with_crop_or_pad(fluo_target, 848, 848)
 
     return bfimg, fluo_target
 
@@ -51,10 +61,11 @@ def main():
     number_of_slices = 5
     bs = 1
 
-    df_train = create_image_path_df("Data/Processed/spheroids_png_crop")
+    df_train = create_image_path_df("Data/Procssed/spheroids_png_crop")
     df_train['plane'] = df_train['brightfield'].apply(lambda x: int(x.split('-')[0].split('p')[-1]))
     df_train = df_train[(df_train.plane <= (df_train.plane.max() - (number_of_slices//2))) & (df_train.plane >= (df_train.plane.min() + (number_of_slices//2)))]
     df_train['number_of_slices'] = number_of_slices
+    df_train['plane_string'] = df_train['brightfield'].apply(lambda x: str(x.split('-')[0].split('f01')[-1]))
     df_train = df_train.astype({col: 'int32' for col in df_train.select_dtypes('int64').columns})
 
     training_df = df_train[0:26]    # NOTE: this is ONLY for the pilot
@@ -63,8 +74,11 @@ def main():
     ds_train = tf.data.Dataset.from_tensor_slices((training_df['nucleus'],
                                                     training_df['brightfield'], 
                                                     training_df['plane'],
-                                                    training_df['number_of_slices']))
+                                                    training_df['number_of_slices'],
+                                                    training_df['plane_string']))
     ds_train = ds_train.map(read_imgs)
+    # for elem in ds_train.take(10):
+    #     print(elem)
     ds_train = ds_train.batch(batch_size = bs, drop_remainder = True)
     ds_train = ds_train.repeat()
     ds_train = ds_train.prefetch(buffer_size = tf.data.AUTOTUNE)
@@ -72,7 +86,8 @@ def main():
     ds_test = tf.data.Dataset.from_tensor_slices((testing_df['nucleus'],
                                                     testing_df['brightfield'], 
                                                     testing_df['plane'],
-                                                    testing_df['number_of_slices']))
+                                                    testing_df['number_of_slices'],
+                                                    training_df['plane_string']))
     ds_test = ds_test.map(read_imgs)
     ds_test = ds_test.batch(batch_size = bs, drop_remainder = True)
     ds_test = ds_test.prefetch(buffer_size = tf.data.AUTOTUNE) 
